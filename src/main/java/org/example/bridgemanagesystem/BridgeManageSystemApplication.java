3db9573dcc134f3d7edc3565ccff7ab19b38f713
staycalm <hello@staycalm.cn>
2025-06-26 10:44:27 +0800

    初次提交

---
+++ src/main/java/org/example/bridgemanagesystem/BridgeManageSystemApplication.java
@@ -0,0 +1,14 @@
+package org.example.bridgemanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BridgeManageSystemApplication.class, args);
+    }
+
+
+}
